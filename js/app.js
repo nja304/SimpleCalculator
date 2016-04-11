3fa8ee3fa16c5758fe8f29b26d9bea13bcd6285e
An Do <nja304@gmail.com>
2016-04-11 22:00:08 +0700

    Add updateOperator

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
+var pheptinh = document.getElementById("pheptinh").innerHTML;
 function updatePhepTinh(n){
-    var pheptinh = document.getElementById("pheptinh");
-    pheptinh.innerHTML = n;
+    pheptinh = n;
+}
+
+
+function updateOperator(m){
+    var lastChar=pheptinh.substring(pheptinh.length-1, pheptinh.length);
+    if (lastChar=="+"||lastChar=="-"||lastChar=="*"||lastChar=="/"){
+        pheptinh=pheptinh.substring(0,pheptinh.length-1);
+    }
+    pheptinh=pheptinh+m;
+
 }
