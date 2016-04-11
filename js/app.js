0caf4a0a99a76b38c1a06864975a39630f09e625
Nam Pham <nampdn96@gmail.com>
2016-04-11 22:15:52 +0700

    fix updateOperator issue

--- js/app.js
+++ js/app.js
@@ -1,14 +1,22 @@
-var pheptinh = document.getElementById("pheptinh").innerHTML;
-function updatePhepTinh(n){
-    pheptinh = n;
+var pheptinh = document.getElementById("pheptinh");
+
+function setPhepTinh(s) {
+    pheptinh.innerHTML = s;
+}
+
+function getPhepTinh() {
+    return pheptinh.innerHTML;
+}
+
+function updatePhepTinh(n) {
+    setPhepTinh(getPhepTinh() + n);
 }
 
 
-function updateOperator(m){
-    var lastChar=pheptinh.substring(pheptinh.length-1, pheptinh.length);
-    if (lastChar=="+"||lastChar=="-"||lastChar=="*"||lastChar=="/"){
-        pheptinh=pheptinh.substring(0,pheptinh.length-1);
+function updateOperator(m) {
+    var lastChar = (getPhepTinh()).substring(getPhepTinh().length - 1, getPhepTinh().length);
+    if (lastChar == "+" || lastChar == "-" || lastChar == "*" || lastChar == "/") {
+        setPhepTinh(getPhepTinh().substring(0, getPhepTinh().length - 1));
     }
-    pheptinh=pheptinh+m;
-
+    setPhepTinh(getPhepTinh() + m);
 }
